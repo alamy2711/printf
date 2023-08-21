1ba7bae072d95fdb21ded87ee0daa2c992c023e2
Omar-Driouch <omardriouch29@gmail.com>
2023-08-21 13:35:03 +0100

    update main.h

--- main.h
+++ main.h
@@ -21,15 +21,21 @@ typedef struct formatter
 
 /* Prototypes */
 int _printf(const char *format, ...);
+int (*get_right_func(const char *s))(va_list);
 int _putchar(char c);
+int print_number(long num);
+int printf_x_or_X(unsigned int num, int IsLower);
+
+/* Specifiers functions */
 int printf_s(va_list);
 int printf_i(va_list);
-int printf_c(va_list c);
-int printf_b(va_list b);
-int printf_o(va_list o);
-
-int print_number(long num);
-
-int (*get_right_func(const char *s))(va_list);
+int printf_c(va_list);
+int printf_b(va_list);
+int printf_o(va_list);
+int printf_u(va_list);
+int printf_x(va_list);
+int printf_X(va_list);
+int printf_S(va_list);
+int printf_p(va_list);
 
 #endif /* MAIN_H */
