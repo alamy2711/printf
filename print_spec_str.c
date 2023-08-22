0a301d8a0f463119f152211f8bf256c532f686d6
Alamy <mestafaalami11@gmail.com>
2023-08-22 22:41:49 +0000

    Update print_spec_str.c

--- print_spec_str.c
+++ print_spec_str.c
@@ -84,3 +84,71 @@ int printf_S(va_list S)
 	}
 	return (length);
 }
+
+/**
+ * printf_r - Prints a string in reverse
+ * @r: String to print in reverse
+ *
+ * Return: Length of the printed string
+ */
+int printf_r(va_list r)
+{
+	char *str = va_arg(r, char *);
+	int i = 0, str_len = 0, len = 0;
+
+	if (str == NULL)
+	{
+
+		return (0);
+	}
+
+	while (str[i] != '\0')
+	{
+		i++;
+		str_len++;
+	}
+
+	for (i = str_len - 1; i >= 0; i--)
+		len += _putchar(str[i]);
+
+	return (len);
+}
+
+/**
+ * printf_R - Print a string with ROT13 encoding.
+ *
+ * This function takes a string as a variable argument and prints it to the
+ * standard output with ROT13 encoding. ROT13 is a simple letter substitution
+ * cipher that replaces each letter with the letter 13 positions forward in the
+ * alphabet. Non-alphabet characters are left unchanged.
+ *
+ * @R: A va_list containing the string to be printed with ROT13 encoding.
+ *
+ * Return: The number of characters printed, excluding the null-terminator.
+ */
+
+int printf_R(va_list R)
+{
+	char *s = va_arg(R, char *);
+	char chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	if (s == NULL)
+		return (0);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 26 * 2; j++)
+		{
+			if (s[i] == chars[j])
+			{
+				_putchar(rot13[j]);
+				break;
+			}
+		}
+		if (j == 26 * 2)
+			_putchar(s[i]);
+	}
+	return (i);
+}
