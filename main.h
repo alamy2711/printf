3c353f6e8bbce3c7bb063b8f18c1d3628826b424
Alamy <mestafaalami11@gmail.com>
2023-08-22 22:39:39 +0000

    Update main.h

--- main.h
+++ main.h
@@ -22,9 +22,11 @@ typedef struct formatter
 /* Prototypes */
 int _printf(const char *format, ...);
 int (*get_right_func(const char *s))(va_list);
+int get_flag(const char *str, int *i, va_list ls);
 int _putchar(char c);
 int print_number(long num);
 int printf_x_or_X(unsigned int num, int IsLower);
+int handleLH(const char *s, int *i, va_list ar_ls);
 
 /* Specifiers functions */
 int printf_s(va_list);
@@ -37,5 +39,7 @@ int printf_x(va_list);
 int printf_X(va_list);
 int printf_S(va_list);
 int printf_p(va_list);
+int printf_r(va_list);
+int printf_R(va_list);
 
 #endif /* MAIN_H */
