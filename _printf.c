35ffb54106afd06ffa62966ed9606883189b86c5
Omar-Driouch <omardriouch29@gmail.com>
2023-08-22 23:27:23 +0100

    updated _printf

--- _printf.c
+++ _printf.c
@@ -1,36 +1,39 @@
 #include "main.h"
 /**
  * _printf - Custom implementation of the printf function
- * @format: A pointer to a null-terminated format string.
+ * @f: A pointer to a null-terminated f string.
  * Return: The total number of characters printed, or -1 if an error occurs.
  */
-int _printf(const char *format, ...)
+int _printf(const char *f, ...)
 {
 	va_list args;
-	char spec[] = {'i', 'd', 'c', 's', 'b', 'o', 'u', 'x', 'X', 'S', 'p', '\0'};
+	char spec[] = {'i', 'd', 'c', 's', 'b', 'o', 'u', 'x', 'X', 'S', 'p',
+				   'r', '#', '+', ' ', 'R', 'l', 'h', '\0'};
 	int total_written = 0, i = 0, j = 0;
 
-	if (!format || (format[0] == '%' && !format[1]))
+	if (!f || ((f[0] == '%' && !f[1]) || (f[0] == '%' && f[1] == ' ' && !f[2])))
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	va_start(args, format);
-	while (format[i] != '\0')
+	va_start(args, f);
+	while (f[i] != '\0')
 	{
-		if (format[i] == '%' && format[i + 1] != '\0')
+		if (f[i] == '%' && f[i + 1] != '\0')
 		{
 			for (j = 0; spec[j] != '\0'; j++)
 			{
-				if (format[i + 1] == spec[j])
+				if (f[i + 1] == spec[j])
 				{
-					total_written += get_right_func(format + i + 1)(args);
+					if (f[i + 1] == '#' || f[i + 1] == ' ' ||
+					 f[i + 1] == '+' || f[i + 1] == 'l' || f[i + 1] == 'h')
+						total_written += get_flag(f, &i, args);
+					else
+						total_written += get_right_func(f + i + 1)(args);
 					i += 2;
 					break;
 				}
 			}
 			if (spec[j] == '\0')
 			{
-				if (format[i + 1] == '%')
+				if (f[i + 1] == '%')
 					i++;
 				goto print_me;
 			}
@@ -38,8 +41,7 @@ int _printf(const char *format, ...)
 		else
 		{
 print_me:
-			total_written += _putchar(format[i]);
-			i++;
+			total_written += _putchar(f[i++]);
 		}
 	}
 	va_end(args);
