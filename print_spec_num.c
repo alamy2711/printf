78e9966e926c6c866d40c385db4ee0552fc7d3ff
Omar-Driouch <omardriouch29@gmail.com>
2023-08-22 23:32:04 +0100

    updated printf spec num

--- print_spec_num.c
+++ print_spec_num.c
@@ -82,3 +82,116 @@ int printf_p(va_list p)
 
 	return (len);
 }
+
+/**
+ * get_flag - Extract and process formatting flags from a format string.
+ *
+ * This function parses a format string for a specific flag
+ * character (e.g., '+',
+ * ' ' or flag for integers like 'd', 'i', 'o', 'x', 'X') starting from the
+ * given index and processes it. It handles the flag character and optional
+ *  space
+ * or plus sign, making appropriate function calls based on the detected flag
+ *
+ * @s:  A pointer to the format string being processed.
+ * @i:    The index in the format string where the flag character is located
+ * @ar_ls: a va_list containing the variable arguments for formatting.
+ *
+ * Return: The updated index in the format string after processing the flag.
+ */
+
+int get_flag(const char *s, int *i, va_list ar_ls)
+{
+	int keepI = *i, n = *i, num, lent = 0, w = 0;
+	va_list copy;
+
+	va_copy(copy, ar_ls);
+	num = va_arg(copy, int);
+	if (s[n + 1] == ' ' && s[n + 2] == '%')
+	{
+		*i += 2;
+		return (_putchar('%'));
+	}
+	while (s[n + 1] == '#' || s[n + 1] == ' ' || s[n + 1] == '+')
+	{
+		if (s[n + 2] == 'd' || s[n + 2] == 'i')
+		{
+			lent += (s[n + 1] == '+' && num >= 0) ? _putchar('+') :
+			 (s[n + 1] == ' ' && num >= 0) ? _putchar(' ') : 0;
+			lent += printf_i(ar_ls);
+		}
+		else if (s[n + 2] == 'o')
+		{
+			s[n + 2] == 'o' && s[n + 1] != ' ' && s[n + 1] != '+' ? _putchar('0') : w++;
+			lent += printf_o(ar_ls);
+		}
+		else if (s[n + 2] == 'x')
+		{
+			s[n + 2] == 'x' && s[n + 1] != ' ' && s[n + 1] != '+' ? _printf("0x") : w++;
+			lent += printf_x(ar_ls);
+		}
+		else if (s[n + 2] == 'X')
+		{
+			s[n + 2] == 'X' && s[n + 1] != ' ' && s[n + 1] != '+' ? _printf("0X") : w++;
+			lent += printf_X(ar_ls);
+		}
+		n++;
+	}
+	n = (lent == 0) ? keepI : n;
+	*i = n;
+	handleLH(s, i, ar_ls);
+	return (lent);
+}
+
+
+
+/**
+ * handleLH - Length Modifiers 'l' and 'h' in a Format String
+ *
+ * This function processes the 'l' and 'h' length modifiers in a format string
+ * and calls the appropriate printf-like function to format and print the
+ * corresponding argument based on the modifiers.
+ *
+ * @s:     The format string to parse.
+ * @i:     A pointer to the current position in the format string.
+ * @ar_ls: The va_list of arguments.
+ *
+ * Return: The total number of characters printed by the called functions.
+ */
+
+
+
+int handleLH(const char *s, int *i, va_list ar_ls)
+{
+
+	int n = *i, lent = 0;
+
+	while (s[n + 1] == 'l' || s[n + 1] == 'h')
+	{
+		if (s[n + 2] == 'd' || s[n + 2] == 'i')
+		{
+
+			lent += printf_i(ar_ls);
+		}
+		if (s[n + 2] == 'o')
+		{
+
+			lent += printf_o(ar_ls);
+		}
+		if (s[n + 2] == 'x')
+		{
+
+			lent += printf_x(ar_ls);
+		}
+		if (s[n + 2] == 'X')
+		{
+
+			lent += printf_X(ar_ls);
+		}
+		n++;
+	}
+
+	*i = n;
+
+	return (lent);
+}
